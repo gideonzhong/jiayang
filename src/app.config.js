1668fe731f0fe291c0a734a14f03a87b79b37ee1
ckchung <541464975@qq.com>
2018-06-12 21:05:23 +0800

    aaaa

--- src/app.config.js
+++ src/app.config.js
@@ -4,13 +4,4 @@ window.__sealtalk_config={
   //appUrl:"http://192.168.0.117:8088",//朱
   // appUrl:"http://192.168.0.168:8082",//线下田
   // appUrl:"http://123.206.108.84:8099",//测试用
-  appUrl:"https://www.chaohuo.net:7090",//线上
-  appkey:"x4vkb1qpx2v3k"
 }
-//app-secret   ukSzoHUX6o
-//融云生产appkey
-//appKey = "x4vkb1qpx2v3k"
-//appSecret = "TZ6tVZSZbOMNNT"
-
-
-//  appkey:"x18ywvqfxn8rc"
